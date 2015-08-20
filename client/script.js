3e73ac3f30c728b1f6c1f63b359b0f8bef544b39
Teodor Cataniciu <teodor.cataniciu@gmail.com>
2015-08-19 19:03:27 -0700

    time and weather api set up

--- client/script.js
+++ client/script.js
@@ -34,11 +34,39 @@ myApp.config(['$routeProvider', function($routeProvider) {
 		redirectTo: '/'
 	});
 }]);
-
+// ******************CREATE A TIME DIRECTIVE************************************
+myApp.directive("myCurrentTime", function(dateFilter){
+    return function(scope, element, attrs){
+        var format;
+        scope.$watch(attrs.myCurrentTime, function(value) {
+            format = value;
+            updateTime();
+        });
+        
+        function updateTime(){
+            var dt = dateFilter(new Date(), format);
+            element.text(dt);
+        }
+        
+        function updateLater() {
+            setTimeout(function() {
+              updateTime(); // update DOM
+              updateLater(); // schedule another update
+            }, 1000);
+        }
+        
+        updateLater();
+    }
+});
 //****************** Factory for all Controllers *********************
 myApp.factory('mirrorFactory', function($http){
 	var factory={};
 	var name;
+	factory.getWeather = function(callback){
+		$http.get('http://api.apixu.com/v1/forecast.json?key=7b67e669b24a4e699f9173147151908&q=San_Jose&days=2').success(function(res){
+			callback(res);
+		})
+	}
 	factory.addItem = function(data,callback){
 		$http.post('/items/new',data).success(function(res){
 			callback(res);
@@ -75,12 +103,71 @@ myApp.factory('mirrorFactory', function($http){
 	return factory;
 })
 //******************  Controller for Mirror **********************************************
-myApp.controller('mirrorController',function($scope,mirrorFactory){
-	$scope.time = time;
+myApp.controller('mirrorController',function($scope,mirrorFactory,$interval){
+	$scope.format = 'h:mm a';
+	var updateWeather = function()
+	{
+		mirrorFactory.getWeather(function(data){
+		$scope.weather = data
+		console.log("we refreshed weather",$scope.weather)
+//****************************************************************************************	
+	// Set up date format for the mirror
+	//Retrieve current date from weather API 
+		var date = $scope.weather.forecast.forecastday[0].date;
+	//Transform month from number to string
+		if(date[5] == 0){
+			var month = monthNames[date[6]-1];
+		}
+		else{
+			var month = monthNames[date[5]+date[6]];
+		}
+		//send the formatted date for display
+	$scope.dateFormatted = date[date.length-2]+date[date.length-1]+' '+month+' '+date.substring(0,4);
+//*******************************************************************************************
+		//Format Weather Information
+		//Select Icon
+		var code = $scope.weather.current.condition;
+		var icon_name;
+		var timeCheck = $scope.weather.current.last_updated;
+		var timeLogic = timeCheck.substring(10,13);
+		var checkTime = function(time)
+		{
+			if(time>20 || time<6)
+			{
+				return "night"
+			}
+			else{
+				return "day"
+			}
+		}
+		var timeOfDay = checkTime(timeLogic);
+		if(code.code == 1000 && timeOfDay == "day")
+		{
+			$scope.icon_name = "wi wi-day-sunny"
+		}
+		else if(code.code == 1000 && timeOfDay == "night")
+		{
+			$scope.icon_name = "wi wi-night-clear"
+		}
+		else if((code.code == 1003 || code.code == 1006) && timeOfDay == "day")
+		{
+			$scope.icon_name = "wi wi-day-cloudy"
+		}
+		else if((code.code == 1003 || code.code == 1006) && timeOfDay == "night")
+		{
+			$scope.icon_name = "wi wi-night-alt-cloudy"
+		}
+		else
+		{
+			$scope.icon_name = "wi wi-hail"
+		}
+		});	
+	}
+	updateWeather();
+	$interval(updateWeather,1200000);
 	})
 // ************************Login Controller ********************************
 myApp.controller('loginController',function($scope,mirrorFactory){
-	console.log('loginController initialized');
 	//Set the user in factory from the login form 
 	$scope.setUser = function(){
 			mirrorFactory.setUser($scope.newUser);
